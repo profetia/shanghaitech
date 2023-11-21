a91a1dd9859d7192fee59df477cea9809c188549
yanglinshu <nicolasvandarkholme@outlook.com>
2023-11-21 12:32:54 +0800

    perf: use thrust instead of by hand

--- include/cuckoohash/set.cuh
+++ include/cuckoohash/set.cuh
@@ -2,6 +2,9 @@
 #define CUCKOOHASH_SET_H_
 
 #include <thrust/device_vector.h>
+#include <thrust/execution_policy.h>
+#include <thrust/functional.h>
+#include <thrust/transform_reduce.h>
 
 #include <cstddef>
 #include <cstdint>
@@ -17,50 +20,46 @@ namespace cuckoohash::set {
 namespace detail {
 
 template <const std::size_t T, const std::size_t C, const std::size_t U>
-__device__ bool insert(slot::SlotView<T, C>& slot, std::uint32_t rehash, std::uint32_t key) {
-  std::size_t reloc = 0;
-  while (reloc < U) {
-    auto hash = hash::Hash(rehash + reloc % T)(key);
-    auto old = atomicExch(&slot.keys[reloc % T][hash % C], static_cast<int32_t>(key));
-    if (old == slot::kEmpty || old == key) {
-      return true;
+struct InsertImpl : public thrust::unary_function<std::uint32_t, bool> {
+  InsertImpl(slot::SlotView<T, C> slot, std::uint32_t rehash)
+      : slot_(std::move(slot)), rehash_(rehash) {}
+
+  __device__ bool operator()(std::uint32_t key) const {
+    std::size_t reloc = 0;
+    while (reloc < U) {
+      auto hash = hash::Hash(rehash_ + reloc % T)(key);
+      auto old = atomicExch(&slot_.keys[reloc % T][hash % C], static_cast<int32_t>(key));
+      if (old == slot::kEmpty || old == key) {
+        return true;
+      }
+      ++reloc;
+      key = old;
     }
-    ++reloc;
-    key = old;
+    return false;
   }
-  return false;
-}
+
+  slot::SlotView<T, C> slot_;
+  std::uint32_t rehash_;
+};
 
 template <const std::size_t T, const std::size_t C, const std::size_t U>
-__global__ void insert(slot::SlotView<T, C> slot, std::uint32_t rehash, std::uint32_t* colide,
-                       const uint32_t* keys, const std::size_t size) {
-  for (std::size_t i = blockIdx.x * blockDim.x + threadIdx.x; i < size && *colide == 0;
-       i += blockDim.x * gridDim.x) {
-    if (!insert<T, C, U>(slot, rehash, keys[i])) {
-      atomicAdd(colide, 1);
-    }
-  }
-}
+struct LookupImpl : public thrust::unary_function<std::uint32_t, bool> {
+  LookupImpl(slot::SlotView<T, C> slot, std::uint32_t rehash)
+      : slot_(std::move(slot)), rehash_(rehash) {}
 
-template <const std::size_t T, const std::size_t C>
-__device__ bool lookup(slot::SlotView<T, C>& slot, std::uint32_t rehash, std::uint32_t key) {
-  for (std::size_t i = 0; i < T; ++i) {
-    auto hash = hash::Hash(rehash + i)(key);
-    if (slot.keys[i][hash % C] == key) {
-      return true;
+  __device__ bool operator()(std::uint32_t key) const {
+    for (std::size_t i = 0; i < T; ++i) {
+      auto hash = hash::Hash(rehash_ + i)(key);
+      if (slot_.keys[i][hash % C] == key) {
+        return true;
+      }
     }
+    return false;
   }
-  return false;
-}
 
-template <const std::size_t T, const std::size_t C>
-__global__ void lookup(slot::SlotView<T, C> slot, std::uint32_t rehash, bool* result,
-                       const uint32_t* keys, const std::size_t size) {
-  for (std::size_t i = blockIdx.x * blockDim.x + threadIdx.x; i < size;
-       i += blockDim.x * gridDim.x) {
-    result[i] = lookup<T, C>(slot, rehash, keys[i]);
-  }
-}
+  slot::SlotView<T, C> slot_;
+  std::uint32_t rehash_;
+};
 
 }  // namespace detail
 
@@ -75,17 +74,11 @@ class Set {
   void insert(thrust::device_vector<std::uint32_t>& keys) {
     auto view = slot::SlotView<T, C>(slot_);
     for (std::size_t i = 0; i < builtin::kRehashLimit; ++i) {
-      std::uint32_t* colide;
-      cudaMalloc(&colide, sizeof(std::uint32_t));
-      cudaMemset(colide, 0, sizeof(std::uint32_t));
-      detail::insert<T, C, U>
-          <<<(keys.size() + builtin::kBlockSize - 1) / builtin::kBlockSize, builtin::kBlockSize>>>(
-              view, rehash_ + static_cast<std::uint32_t>(i), colide, keys.data().get(),
-              keys.size());
-      std::uint32_t colide_host;
-      cudaMemcpy(&colide_host, colide, sizeof(std::uint32_t), cudaMemcpyDeviceToHost);
-      cudaFree(colide);
-      if (colide_host == 0) {
+      bool success = thrust::transform_reduce(
+          thrust::device, keys.begin(), keys.end(),
+          detail::InsertImpl<T, C, U>(view, rehash_ + static_cast<std::uint32_t>(i)), true,
+          thrust::logical_and<bool>());
+      if (success) {
         rehash_ += i;
         return;
       }
@@ -101,9 +94,8 @@ class Set {
   thrust::device_vector<bool> lookup(thrust::device_vector<std::uint32_t>& keys) {
     auto result = thrust::device_vector<bool>(keys.size());
     auto view = slot::SlotView<T, C>(slot_);
-    detail::lookup<T, C>
-        <<<(keys.size() + builtin::kBlockSize - 1) / builtin::kBlockSize, builtin::kBlockSize>>>(
-            view, rehash_, result.data().get(), keys.data().get(), keys.size());
+    thrust::transform(thrust::device, keys.begin(), keys.end(), result.begin(),
+                      detail::LookupImpl<T, C, U>(view, rehash_));
     return result;
   }
 
